d2f324dbcc6acf108f5f7220609963d49d133906
Arthur Suzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-10-02 21:12:10 -0300

    Update settings.py

--- ECOnnect/Projeto/settings.py
+++ ECOnnect/Projeto/settings.py
@@ -29,8 +29,8 @@ if NOT_PROD:
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
     # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = 'A SECRET KEY DO SEU PROJETO'
-    ALLOWED_HOSTS = [*]
+    SECRET_KEY = 'django-insecure-1jb1wip@qo&36c4d_^=lhno4d#7ijj61n*4-s!x*lf^wvz1vfk'
+    ALLOWED_HOSTS = []
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
