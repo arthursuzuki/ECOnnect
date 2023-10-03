05c1b695008681c3ddfdfca658fc311be59f046a
Arthur Suzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-10-02 21:47:29 -0300

    Update urls.py

--- ECOnnect/Budget/urls.py
+++ ECOnnect/Budget/urls.py
@@ -4,7 +4,7 @@ from Budget.views import sobreNos, empresas, area, potencial
 
 urlpatterns = [
     path('orcamento/', orcamento, name='orcamento'),  # Orçamento
-    path('', area),  # Área Disponível
+    path('', area,name=''),  # Área Disponível
     path('calculadora/', calculadora,name='calculadora'),  # Calculadora Solar
     path('suporte/', suporte,name='suporte'),  # Contato
     path('informacoes/', informacoes,name='informacoes'),  # Informações
