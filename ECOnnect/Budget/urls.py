d5cb214b8381368610c13b9f3272f897da47b3e2
Eduardo Alves Lins <140346484+eduardoalins@users.noreply.github.com>
2023-10-02 21:52:55 -0300

    Update urls.py
    aspas duplas

--- ECOnnect/Budget/urls.py
+++ ECOnnect/Budget/urls.py
@@ -3,12 +3,12 @@ from Budget.views import orcamento, suporte, calculadora, informacoes
 from Budget.views import sobreNos, empresas, area, potencial
 
 urlpatterns = [
-    path('orcamento/', orcamento, name='orcamento'),  # Orçamento
-    path('', area,name=''),  # Área Disponível
-    path('calculadora/', calculadora,name='calculadora'),  # Calculadora Solar
-    path('suporte/', suporte,name='suporte'),  # Contato
-    path('informacoes/', informacoes,name='informacoes'),  # Informações
-    path('sobrenos/', sobreNos,name='sobrenos'),  # Sobre Nós
-    path('empresas/', empresas,name='empresas'),  # Empresas Próximas A Mim
-    path('potencial/', potencial,name='potencial'),  # Potencial de Geração de Energia
+    path("orcamento/", orcamento, name='orcamento'),  # Orçamento
+    path("", area,name=''),  # Área Disponível
+    path("calculadora/", calculadora,name='calculadora'),  # Calculadora Solar
+    path("suporte/", suporte,name='suporte'),  # Contato
+    path("informacoes/", informacoes,name='informacoes'),  # Informações
+    path("sobrenos/", sobreNos,name='sobrenos'),  # Sobre Nós
+    path("empresas/", empresas,name='empresas'),  # Empresas Próximas A Mim
+    path("potencial/", potencial,name='potencial'),  # Potencial de Geração de Energia
 ]
