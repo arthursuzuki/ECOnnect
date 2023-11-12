33adf6707fac36606eb5417ef10358cca12dc2c5
lauravidall <132401057+lauravidall@users.noreply.github.com>
2023-11-12 13:34:13 -0300

    Update urls.py

--- ECOnnect/Budget/urls.py
+++ ECOnnect/Budget/urls.py
@@ -1,14 +1,24 @@
 from django.urls import path
-from Budget.views import orcamento, suporte, calculadora, informacoes
-from Budget.views import sobreNos, empresas, area, potencial
+
+from Budget.views import (area, calculadora, empresas, informacaosolar,
+                          orcamento, potencial, sobreNos, suporte)
+
+from . import views
 
 urlpatterns = [
-    path('orcamento/', orcamento),  # Orçamento
-    path('', area),  # Área Disponível
-    path('calculadora/', calculadora),  # Calculadora Solar
-    path('suporte/', suporte),  # Contato
-    path('informacoes/', informacoes),  # Informações
-    path('sobreNos/', sobreNos),  # Sobre Nós
-    path('empresas/', empresas),  # Empresas Próximas A Mim
-    path('potencial/', potencial),  # Potencial de Geração de Energia
+    path('orcamento', views.orcamento, name='orcamento'),  # Orçamento
+    path('faq', views.faq, name='faq'),  # Fale conosco
+    path('login', views.login, name='login'),  # Login
+    path('cadastro', views.cadastro, name='cadastro'),  # Cadastro
+    path('area/', views.area, name='area'),  # Área Disponível
+    path('infocredito', views.infocredito, name='infocredito'),  # Créditos de Carbono
+    path('', views.home, name='home'),  # Home
+    path('calculadora', views.calculadora, name='calculadora'),  # Calculadora Solar
+    path('suporte', views.suporte, name='suporte'),  # Contato
+    path('informacoes', views.informacaosolar, name='informacoes'),  # Informações
+    path('sobreNos', views.sobreNos, name='sobreNos'),  # Sobre Nós
+    path('empresaprox', views.empresaprox, name='empresaprox'), # Empresas próximas a mim
+    path('empresas', views.empresas, name='empresas'),  # Empresas Próximas A Mim
+    path('potencial', views.potencial, name='potencial'),  # Potencial de Geração de Energia
+    path('informacaosolar', views.informacaosolar, name='informacaosolar'),  # Informação Solar
 ]
