386f0e78706245b6d537386c0baf8ec5e62ba58b
Arthur Suzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-11-12 11:56:45 -0300

    Update forms.py

--- ECOnnect/Budget/forms.py
+++ ECOnnect/Budget/forms.py
@@ -5,3 +5,8 @@ class ContatoForm(forms.ModelForm):
     class Meta:
         model = Contato
         fields = ['nome', 'celular', 'email', 'mensagem']
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['nome', 'sobrenome','cidade','estado', 'nota', 'texto']
