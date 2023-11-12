1eb30ea1bf0d9d27e991db3dec9c8f206298d867
lauravidall <132401057+lauravidall@users.noreply.github.com>
2023-11-12 13:34:37 -0300

    Update views.py

--- ECOnnect/Budget/views.py
+++ ECOnnect/Budget/views.py
@@ -1,30 +1,92 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from .forms import ContatoForm
 
 
 # Create your views here.
 def orcamento(request):
-    return render(request, 'global/orcamento.html', context={
+    return render(request, 'orcamento.html', context={
         'name': 'Cálculo Orçamento'
+    })
+
+def home(request):
+    return render(request, 'home.html', context={
+        'name': 'ECONNECT'
+    })
+
+def empresaprox(request):
+    return render(request, 'empresaprox.html', context={
+        'name': 'Empresa próxima'
     })
 
 
 def area(request):
-    return render(request, 'global/area.html', context={
+    return render(request, 'area.html', context={
         'name': 'Área Disponível'
     })
 
 
 def potencial(request):
-    return render(request, 'global/potencial.html', context={
+    return render(request, 'potencial.html', context={
         'name': 'Potencial de Geração de Energia'
     })
 
 
 def calculadora(request):
-    return render(request, 'global/calculadora.html', context={
+    return render(request, 'calculadora.html', context={
         'name': 'Cálculo Orçamento'
     })
+
+def infocredito(request):
+    return render(request, 'infocredito.html', context={
+        'name': 'Créditos de Carbono'
+    })
+
+def login(request):
+    return render(request, 'login.html', context={
+        'name': 'Login'
+    })
+
+def cadastro(request):
+    return render(request, 'cadastro.html', context={
+        'name': 'Cadastro'
+    })
+
+def informacaosolar(request):
+    roi = None
+
+    if request.method == 'POST':
+        tipo_local = request.POST.get('tipo_local')
+        gasto_mensal = float(request.POST.get('gasto_mensal').replace('R$', '').replace(',', '').strip())
+
+        if tipo_local == 'Residencial':
+            custo_instalacao = 5000
+        elif tipo_local == 'Comercial':
+            custo_instalacao = 10000
+        elif tipo_local == 'Industrial':
+            custo_instalacao = 20000
+        else:
+            return HttpResponse("Tipo de local inválido.")
+
+        economia_anual = gasto_mensal * 12
+
+        if custo_instalacao > 0:
+            roi = (economia_anual / custo_instalacao) * 100
+        else:
+            roi = 0
+
+    return render(request, 'informacaosolar.html', {'name': 'Informação Solar', 'roi': roi})
+
+def faq(request):
+    if request.method == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = ContatoForm()
+    return render(request,"faq.html",{'form':form})
 
 
 def suporte(request):
