c0cb1ec108c4428c210a78cd42374543d3a5c405
Arthur Suzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-11-26 20:33:04 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,12 @@
 from django.test import TestCase
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
 class SeuTeste(TestCase):
     def setUp(self):
-        self.servico = Service(ChromeDriverManager().install())
-        self.navegador = webdriver.Chrome(service=self.servico)
+        self.navegador = webdriver.Chrome(ChromeDriverManager().install())
         self.navegador.get('http://127.0.0.1:8000/')
         time.sleep(2)
 
